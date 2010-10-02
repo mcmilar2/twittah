dcde58e9d09c9d0c4494aebbe9ae8e997cb04f48
Alex McMillan <mcmilar2@student.op.ac.nz>
2010-10-02 15:28:17 +1300

    Trying to setup pages--about route

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,4 +7,7 @@ class PagesController < ApplicationController
 
   def about
   end
+
+  def foobar
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Twittah::Application.routes.draw do
-  get "pages/home"
   get "pages/about"
+  get "pages/home"
   get "pages/contact"
 
 
@@ -53,7 +53,7 @@ Twittah::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+ root :to => "pages#home"
 
   # See how all your routes lay out with "rake routes"
 
