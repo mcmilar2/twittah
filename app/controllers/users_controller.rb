fa2afae9a6e6f11397c29eb38e72db4b362749a8
Alex McMillan <br4inz@orcon.net.nz>
2010-10-08 21:51:54 +1300

    Working users model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,8 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new(params[:user])
 		if @user.save
-			# HANDLE A SUCCESSFUL SAVE
+			flash[:success] = "Welcome to twittah!"
+			redirect_to @user
 		else
 			@title = "Sign Up!"
 			render 'new'
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -6,9 +6,9 @@ describe UsersController do
 	describe "POST 'create'" do
 		describe "failure" do
 			before(:each) do
-				@attr = {	:name				=> "",
-							:email 				=> "",
-							:password			=> "",
+				@attr = {	:name					=> "",
+							:email 					=> "",
+							:password				=> "",
 							:password_confirmation 	=> "" }
 			end
 
@@ -26,6 +26,31 @@ describe UsersController do
 			it "should render the 'new' page" do
 				post :create, :user => @attr
 				response.should render_template('new')
+			end
+		end
+		
+		describe "success" do
+			before(:each) do
+				@attr = {	:name					=> "New User",
+							:email					=> "user@example.com",
+							:password				=> "foobar",
+							:password_confirmation	=> "foobar" }
+			end
+			
+			it "should create a user" do
+				lambda do
+					post :create, :user => @attr
+				end.should change(User, :count).by(1)
+			end
+		
+			it "should redirect to the user show page" do
+				post :create, :user => @attr
+				response.should redirect_to(user_path(assigns(:user)))
+			end
+
+			it "should have a welcome message" do
+				post :create, :user => @attr
+				flash[:success].should =~ /welcome to twittah/i
 			end
 		end
 	end
